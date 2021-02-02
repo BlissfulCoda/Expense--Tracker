a6e2d488df31375b82dc38d30d5715fb00c69970
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-02-02 19:50:15 +0000

    finished the form component and some ui

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider
+} from '@material-ui/core';
+import Form from './Form/Form';
+
+import useStyles from './styles';
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered By Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance £100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: '1.5em', marginTop: '20px ' }}
+        >
+          {/** InforCard */}
+          Try Saying: Add Income for £50 in Category Salary for Sunday
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/** List */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
