8e458cfa1ae00f6f06437dc0797c75aef85f34d6
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-02-03 21:08:50 +0000

    cleaning up some code

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,29 @@
 import React, { useReducer, createContext } from 'react';
 import contextReducer from './contextReducer';
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [
+  {
+    amount: 20,
+    category: 'Travel',
+    type: 'Income',
+    date: '2021-02-06',
+    id: '5b627a4e-2960-44c1-ac87-06e316978572'
+  },
+  {
+    amount: 50,
+    category: 'Car',
+    type: 'Income',
+    date: '2021-02-05',
+    id: '21c1e1bb-7689-48be-9745-0b4803afd0fd'
+  },
+  {
+    amount: 100,
+    category: 'House',
+    type: 'Income',
+    date: '2021-02-05',
+    id: 'f679d421-6a23-4b7a-9e63-8d0b0d1ed6f4'
+  }
+];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
@@ -12,6 +34,14 @@ export const Provider = ({ children }) => {
     dispatch({ type: 'DELETE_TRANSACTION', payload: id });
   const addTransaction = transaction =>
     dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
+
+  const balance = transactions.reduce(
+    (acc, currvalue) =>
+      currvalue.type === 'Expense'
+        ? acc - currvalue.amount
+        : acc + currvalue.amount,
+    0
+  );
 
   return (
     <ExpenseTrackerContext.Provider
--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,13 +1,16 @@
 const contextReducer = (state, action) => {
-    let transactions
+  let transactions;
   switch (action.type) {
     case 'DELETE_TRANSCATION':
       transactions = state.filter(t => t.id !== action.payload);
 
+      localStorage.setItem('transactions', JSON.stringify(transactions));
+
       return transactions;
     case 'ADD_TRANSACTION':
       transactions = [action.payload, ...state];
-      return transactions ;
+      localStorage.setItem('transactions', JSON.stringify(transactions));
+      return transactions;
     default:
       return state;
   }
